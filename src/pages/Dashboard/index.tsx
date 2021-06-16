f87d08ebb986ec3583aa728c30811239c9f613bf
Lucas Luis Ribeiro <lucas.luisribeiro@gmail.com>
2021-06-15 23:23:58 -0300

    Conetando com API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,58 +1,65 @@
-import React from 'react';
+/* eslint-disable camelcase */
+/* eslint-disable prettier/prettier */
+import React, { FormEvent, useState } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
-
+import api from '../../services/api';
 import logoimg from '../../assests/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
+import Repository from '../Repository';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const respository =  response.data;
+
+    setRepositories([...repositories, respository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoimg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
+        {repositories.map(repo => (
+          <a key={repo.full_name} href="teste">
           <img
-            src="https://avatars.githubusercontent.com/u/11708545?v=4"
-            alt="Lucas Luis Ribeiro"
+            src={repo.owner.avatar_url}
+            alt={repo.owner.login}
           />
           <div>
-            <strong>Repositório Lucas</strong>
-            <p>Teste de listagem de repositório</p>
+            <strong>{repo.full_name}</strong>
+            <p>{repo.description}</p>
           </div>
-
           <FiChevronRight size={20} />
         </a>
-        <a href="teste">
-          <img
-            src="https://avatars.githubusercontent.com/u/11708545?v=4"
-            alt="Lucas Luis Ribeiro"
-          />
-          <div>
-            <strong>Repositório Lucas</strong>
-            <p>Teste de listagem de repositório</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars.githubusercontent.com/u/11708545?v=4"
-            alt="Lucas Luis Ribeiro"
-          />
-          <div>
-            <strong>Repositório Lucas</strong>
-            <p>Teste de listagem de repositório</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+        ))}
       </Repositories>
     </>
   );
